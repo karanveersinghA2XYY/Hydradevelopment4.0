91b054af235307887795d163a13552a80059c963
karanveersinghA2XYY <121312976+karanveersinghA2XYY@users.noreply.github.com>
2023-01-11 20:05:34 +0530

    Update chatbot.py

--- Hydra/modules/chatbot.py
+++ Hydra/modules/chatbot.py
@@ -122,7 +122,7 @@ def chatbot(update: Update, context: CallbackContext):
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
         kukiurl = requests.get(
-            f"http://api.brainshop.ai/get?bid=171693&key=qZsmlB6XQGnKnotE&uid=[user]&msg={message}"
+            f"http://api.brainshop.ai/get?bid=170602&key=L95V2CZJnYh1rcxW&uid=[user]&msg={message}"
         )
         
         
